0d6bd36c69624e79ffa438ed6aba7b54d2af018f
nevenxr <nevenxr@gmail.com>
2022-03-25 11:47:18 +0600

    feat(MessageCreate): emit guildMessage on client

--- src/lib/socket/events/MessageCreate.ts
+++ src/lib/socket/events/MessageCreate.ts
@@ -1,9 +1,10 @@
 import { Client } from "../../client/Client";
+import { Message, MessageStructure } from "../../structs/Message";
 import { Socket } from "../Socket";
 
 export default {
     name: "MESSAGE_CREATE",
-    exec: (client: Client, socket: Socket, data: {}) => {
-        
+    exec: (client: Client, socket: Socket, payload: { d: MessageStructure }) => {
+        return client.emit("guildMessage", new Message(client, payload.d));
     },
 };
