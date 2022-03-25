7c80584549cb4c735cf467c9ab878283ab522217
nevenxr <nevenxr@gmail.com>
2022-03-25 11:48:03 +0600

    feat(Client): add types and property apiURL

--- src/lib/client/Client.ts
+++ src/lib/client/Client.ts
@@ -1,11 +1,18 @@
 import { Socket } from "../socket/Socket";
 import { CUser } from "../structs/CUser";
 import EventEmitter from "events";
+import { Message } from "../structs/Message";
+
+export interface Client {
+    on(event: "ready", listener: () => void): this;
+    on(event: "guildMessage", listener: (message: Message) => void): this;
+};
 
 export class Client extends EventEmitter {
     public token: string | null;
     public socket: Socket | null;
     public user: CUser;
+    public apiURL = "https://discord.com/api/v10";
 
     constructor () {
         super();
