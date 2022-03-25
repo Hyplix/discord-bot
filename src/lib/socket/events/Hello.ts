1750ffe99f1cc9036c1809933e7ff275ede046df
nevenxr <nevenxr@gmail.com>
2022-03-25 09:58:58 +0600

    feat(intent): add message intent integer

--- src/lib/socket/events/Hello.ts
+++ src/lib/socket/events/Hello.ts
@@ -10,7 +10,7 @@ export default {
             "op": 2,
             "d": {
                 "token": client.token,
-                "intents": 1 << 0,
+                "intents": 1 << 0 | 1 << 9,
                 "properties": {
                     "$browser": "hyplix",
                     "$device": "hyplix"
