b1674b2a3b62bac8978ce501fbdac3ddee1e02cc
nevenxr <nevenxr@gmail.com>
2022-03-25 09:59:57 +0600

    feat(MessageCreate): add event for messages

---
+++ src/lib/socket/events/MessageCreate.ts
@@ -0,0 +1,9 @@
+import { Client } from "../../client/Client";
+import { Socket } from "../Socket";
+
+export default {
+    name: "MESSAGE_CREATE",
+    exec: (client: Client, socket: Socket, data: {}) => {
+        
+    },
+};
