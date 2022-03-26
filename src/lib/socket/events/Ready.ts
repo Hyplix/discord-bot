76d69b577e8cab30709f25f3df1b23eb10092a34
nevenxr <nevenxr@gmail.com>
2022-03-26 09:39:34 +0600

    feat(Ready): fetch and set guilds

--- src/lib/socket/events/Ready.ts
+++ src/lib/socket/events/Ready.ts
@@ -1,5 +1,7 @@
 import { Payload, Socket } from "../Socket";
 import { Client } from "../../client/Client";
+import fetch from "node-fetch";
+import { Guild } from "../../structs/Guild";
 
 export default {
     name: "READY",
@@ -7,6 +9,19 @@ export default {
         const { d } = data as Payload | any;
         socket.session_id = d.session_id;
         client.user.setProps(d.user);
+
+        d.guilds.forEach(async (g: { unavailable: boolean, id: string }) => {
+            const req = await fetch(client.apiURL + `/guilds/${g.id}`, {
+                headers: {
+                    "Authorization": `Bot ${client.token}`
+                }
+            });
+            
+            if (req.ok) {
+                const res = await req.json();
+                return client.guilds.set(res.id, new Guild(client, res));
+            };
+        });
 
         return client.emit("ready");
     },
