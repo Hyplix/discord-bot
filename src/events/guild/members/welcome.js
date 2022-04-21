da3ce513aac117d02b86721ea45745c90bf984ba
nevenxr <nevenxr@gmail.com>
2022-04-21 23:27:39 +0600

    feat(welcome): add event to welcome members

---
+++ src/events/guild/members/welcome.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "guildMemberAdd",
+    exec: (client, member) => {
+        if (member.user.bot) return;
+        const channel = client.channels.cache.get(client.bot.channel.welcome);
+
+        if (channel) {
+            channel.send(`Hey <@!${member.user.id}> :wave: Welcome to ${member.guild.name}!\n- I hope you enjoy your time here :)`)
+        };
+    },
+};
